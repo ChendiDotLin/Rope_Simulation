aa207e3c086001b5ba4c5a5c13ffc0c1025b7103
Chendi Lin <chendil@weh5201-b2.andrew.cmu.edu>
2018-11-16 20:10:22 -0500

    Lagrange

--- ELE.m
+++ ELE.m
@@ -1,5 +1,18 @@
-function [S] = ELE (L,q,qdot)
+function [S,qddot] = ELE (L,q,qdot)
+    dL_dq = Jacobian(L,q);
+    dL_dqdot =  Jacobian(L,qdot);
     
-
-
+    fq = sym('a',size(q));
+    for i = 1:length(q)
+        fq(i) = symfun(sprintf('fq%d(t)', i), t);
+    end
+    fqdot = diff(fq,t);
+    fqddot = diff(fqdot,t);
+    dL_dqdot = subs(dL_dqdot,[q;qdot],[fq;fqdot]);
+    S = dL_dq - diff(dL_dqdot,t);
+    qddot = sym('qddot',size(q));
+    
+    S = subs(S,[fq;fqdot;fqddot],[q;qdot;qddot]);
+    
+    
 end
